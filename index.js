2c37b7b1b099b7a9e0f7b20fe3332bfdb9842097
Max Gfeller <max.gfeller@gmail.com>
2013-12-16 22:19:07 +0100

    Due to changes to youtube-mp3.org this script no longer worked. They introduced an additional "ping" method and a special sort of code that had to be appended to the URL.

--- index.js
+++ index.js
@@ -17,8 +17,23 @@ downloader.download = function(youtubeLink, filename, finalCallback) {
   var itemInfoLink = this.baseLink + '/a/itemInfo' +
     '/?video_id={0}&ac=www&t=grp';
 
-  var downloadLink = '/get?video_id={0}&h={1}';
+  var pingLink = 'http://ping.aclst.com/ping.php/12790944/{0}';
 
+  var downloadLink = '/get?ab=256&video_id={0}&h={1}&r={2}';
+
+  var _cc = function(a) {
+    var __AM = 65521;
+    if ("string" != typeof a) throw Error("se");
+    var c = 1,
+        b = 0,
+        d, e;
+    for (e = 0; e < a.length; e++) d = a.charCodeAt(e), c = (c + d) % __AM, b = (b + c) % __AM;
+    return b << 16 | c
+  }
+
+  /**
+   * Push the item to their API
+   **/
   var pushItem = function(cb) {
     http.get(pushItemLink, function(res) {
       res.setEncoding('utf8');
@@ -34,6 +49,9 @@ downloader.download = function(youtubeLink, filename, finalCallback) {
     });
   }
 
+  /**
+   * Get the item info link
+   **/
   var getItemInfo = function(token, cb) {
     http.get(itemInfoLink.replace('{0}', token), function(res) {
       res.setEncoding('utf8');
@@ -49,15 +67,31 @@ downloader.download = function(youtubeLink, filename, finalCallback) {
     });
   }
 
-  var downloadFile = function(token, reqBody, cb) {
-    if(reqBody.indexOf('info') !== 0) {
-      cb(reqBody);
-      return;
-    }
-    eval(reqBody);
+  /**
+   * Ping the item on the aclst.com service
+   **/
+  var pingItem = function(token, reqBody, cb) {
+   if(reqBody.indexOf('info') !== 0) return cb('Fail from api: ' + reqBody);
+
+   eval(reqBody);
+
+   http.get(info.pf, function(res) {
+    cb(null, token, info);
+   }).on('error', function(e) {
+     return cb(e.message);
+   });
+  }
+
+  /**
+   * Download the file
+   **/
+  var downloadFile = function(token, info, cb) {
+    var timestamp = (new Date).getTime();
 
     var options = {
-      path: downloadLink.replace('{0}', token).replace('{1}', info.h),
+      path: downloadLink.replace('{0}', token).
+        replace('{1}', info.h).
+        replace('{2}', timestamp + '.' + _cc(token + timestamp)),
       host: this.host,
       agent: false
     };
@@ -80,6 +114,7 @@ downloader.download = function(youtubeLink, filename, finalCallback) {
   runnel(
       pushItem,
       getItemInfo,
+      pingItem.bind(this),
       downloadFile.bind(this),
       function done(err) {
         finalCallback(err);
