bb2226943f001fc012092208e06b5c6c8555c880
MaxGfeller <max.gfeller@gmail.com>
2014-06-14 10:37:41 +0200

    Add function `getDownloadLink`
    If you only want the download link for the mp3 you can use this
    function.

--- index.js
+++ index.js
@@ -9,108 +9,135 @@ var downloader = {};
 downloader.host = 'www.youtube-mp3.org';
 downloader.baseLink = 'http://' + downloader.host;
 
-downloader.download = function(youtubeLink, filename, finalCallback) {
-  var pushItemLink = this.baseLink + '/a/pushItem' +
-    '/?item=' + urlencode(youtubeLink) + 
-    '&el=na&bf=false';
+var pushItemLink = null;
+var itemInfoLink = null;
+var pingLink = 'http://ping.aclst.com/ping.php/12790944/{0}';
+var downloadLink = '/get?ab=256&video_id={0}&h={1}&r={2}';
 
-  var itemInfoLink = this.baseLink + '/a/itemInfo' +
-    '/?video_id={0}&ac=www&t=grp';
+var filename = null;
 
-  var pingLink = 'http://ping.aclst.com/ping.php/12790944/{0}';
+var _cc = function(a) {
+  var __AM = 65521;
+  if ("string" != typeof a) throw Error("se");
+  var c = 1,
+      b = 0,
+      d, e;
+  for (e = 0; e < a.length; e++) d = a.charCodeAt(e), c = (c + d) % __AM, b = (b + c) % __AM;
+  return b << 16 | c
+}
 
-  var downloadLink = '/get?ab=256&video_id={0}&h={1}&r={2}';
+/**
+ * Push the item to their API
+ **/
+var pushItem = function(cb) {
+  http.get(pushItemLink, function(res) {
+    res.setEncoding('utf8');
+    var body = '';
 
-  var _cc = function(a) {
-    var __AM = 65521;
-    if ("string" != typeof a) throw Error("se");
-    var c = 1,
-        b = 0,
-        d, e;
-    for (e = 0; e < a.length; e++) d = a.charCodeAt(e), c = (c + d) % __AM, b = (b + c) % __AM;
-    return b << 16 | c
-  }
-
-  /**
-   * Push the item to their API
-   **/
-  var pushItem = function(cb) {
-    http.get(pushItemLink, function(res) {
-      res.setEncoding('utf8');
-      var body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      res.on('end', function() {
-        cb(null, body);
-      });
+    res.on('data', function(chunk) {
+      body += chunk;
     });
-  }
 
-  /**
-   * Get the item info link
-   **/
-  var getItemInfo = function(token, cb) {
-    http.get(itemInfoLink.replace('{0}', token), function(res) {
-      res.setEncoding('utf8');
-      var body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      res.on('end', function() {
-        cb(null, token, body);
-      });
+    res.on('end', function() {
+      return cb(null, body);
     });
-  }
+  });
+}
 
-  /**
-   * Ping the item on the aclst.com service
-   **/
-  var pingItem = function(token, reqBody, cb) {
-   if(reqBody.indexOf('info') !== 0) return cb('Fail from api: ' + reqBody);
+/**
+ * Get the item info link
+ **/
+var getItemInfo = function(token, cb) {
+  http.get(itemInfoLink.replace('{0}', token), function(res) {
+    res.setEncoding('utf8');
+    var body = '';
 
-   eval(reqBody);
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
 
-   http.get(info.pf, function(res) {
-    cb(null, token, info);
-   }).on('error', function(e) {
-     return cb(e.message);
-   });
-  }
+    res.on('end', function() {
+      cb(null, token, body);
+    });
+  });
+}
 
-  /**
-   * Download the file
-   **/
-  var downloadFile = function(token, info, cb) {
+/**
+ * Ping the item on the aclst.com service
+ **/
+var pingItem = function(token, reqBody, cb) {
+ if(reqBody.indexOf('info') !== 0) return cb('Fail from api: ' + reqBody);
+
+ eval(reqBody);
+
+ http.get(info.pf, function(res) {
+  cb(null, token, info);
+ }).on('error', function(e) {
+   return cb(e.message);
+ });
+}
+
+/**
+ * Download the file
+ **/
+var downloadFile = function(token, info, cb) {
+  var timestamp = (new Date).getTime();
+
+  var options = {
+    path: downloadLink.replace('{0}', token).
+      replace('{1}', info.h).
+      replace('{2}', timestamp + '.' + _cc(token + timestamp)),
+    host: this.host,
+    agent: false
+  };
+
+  var req = http.get(options, function(res) {
+    var writeFile = fs.createWriteStream(filename, {'flags': 'a'});
+    res.on('data', function(chunk) {
+      writeFile.write(chunk, encoding='binary');
+    });
+
+    res.on('end', function() {
+      writeFile.end();
+      cb(null);
+    });
+  }).on('error', function(e) {
+    cb(e);
+  });
+}
+
+var getDownloadLink = function(token, info, cb) {
     var timestamp = (new Date).getTime();
+    var url = this.host + downloadLink.replace('{0}', token).
+      replace('{1}', info.h).
+      replace('{2}', timestamp + '.' + _cc(token + timestamp));
 
-    var options = {
-      path: downloadLink.replace('{0}', token).
-        replace('{1}', info.h).
-        replace('{2}', timestamp + '.' + _cc(token + timestamp)),
-      host: this.host,
-      agent: false
-    };
+    cb(null, url);
+}
 
-    var req = http.get(options, function(res) {
-      var writeFile = fs.createWriteStream(filename, {'flags': 'a'});
-      res.on('data', function(chunk) {
-        writeFile.write(chunk, encoding='binary');
-      });
+downloader.getDownloadLink = function(youtubeLink, finalCallback) {
+    pushItemLink = this.baseLink + '/a/pushItem' +
+      '/?item=' + urlencode(youtubeLink) +
+      '&el=na&bf=false';
+    itemInfoLink = this.baseLink + '/a/itemInfo' +
+      '/?video_id={0}&ac=www&t=grp';
+    runnel(
+        pushItem,
+        getItemInfo,
+        pingItem.bind(this),
+        getDownloadLink.bind(this),
+        function done(err, url) {
+          finalCallback(err, url);
+        });
+}
 
-      res.on('end', function() {
-        writeFile.end();
-        cb(null);
-      });
-    }).on('error', function(e) {
-      cb(e);
-    });
-  }
-
+downloader.download = function(youtubeLink, file, finalCallback) {
+  filename = file;
+  pushItemLink = this.baseLink + '/a/pushItem' +
+    '/?item=' + urlencode(youtubeLink) +
+    '&el=na&bf=false';
+  itemInfoLink = this.baseLink + '/a/itemInfo' +
+    '/?video_id={0}&ac=www&t=grp';
   runnel(
       pushItem,
       getItemInfo,
